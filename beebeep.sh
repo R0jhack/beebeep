438e16513b4e22dedd654e2acb128e3547e93945
cudi1999 <42251302+cudi1999@users.noreply.github.com>
2019-03-12 16:16:44 -0400

    Add files via upload

---
+++ beebeep.sh
@@ -0,0 +1,6 @@
+wget http://downloads.sourceforge.net/project/beebeep/Linux/beebeep-2.0.3-amd64.tar.gz
+tar -zxvf beebeep-2.0.3-amd64.tar.gz
+cd beebeep-2.0.3-amd64/
+sudo chmod +x beebeep
+sudo apt-get install libxcb-screensaver0
+./beebeep
